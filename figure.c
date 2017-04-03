bb4c9186743004209884837ecdb7e5a556357da7
Roby1203 <ioco75501@gmail.com>
2017-04-03 11:41:11 +0200

    Add files via upload

---
+++ figure.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  printf("*********\n");
+  printf("*ecco   *\n");
+  printf("*a voi  * *****\n");
+  printf("*prof   * *   * ***\n");
+  printf("*       * *   * * *\n");
+  printf("*       * *   * * *\n");
+  printf("********* ***** ***\n");
+
+  return 0;
+
+}
