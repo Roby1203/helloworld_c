d3f4021e2d796a19932fb1b7f44c53c572c4913d
Roby1203 <ioco75501@gmail.com>
2017-04-19 12:19:00 +0200

    Update cerchio.c

--- cerchio.c
+++ cerchio.c
@@ -2,11 +2,12 @@
 
 int main(){
   double pi = 3.14;
-  float raggio,circ,super;
+  int raggio;
+  double circ,super;
 
   printf("insersci il raggio \t");
   scanf("%d", &raggio);
-  circ = pi * raggio * 2;
+  circ = pi * raggio * 2.0;
   printf ("la circonferenza e' %f\n", circ);
   super = pi * raggio * raggio;
   printf ("la superficie e' %f\n", super);
